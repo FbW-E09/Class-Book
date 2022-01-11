3beb0ececd2b618641d0c3d69939f369c2230866
HelonDCI <william.roop@digitalcareerinstitute.org>
2022-01-11 07:36:51 -0300

    Added dataTypes

---
+++ PB/language/dataTypes.js
@@ -0,0 +1,23 @@
+// STRINGS - can use double quotes, single quotes, or backticks
+const myString = `It's a Tuesday`;
+
+console.log(typeof myString);
+// console.log(myString.length);
+
+// NUMBERS
+const myNumber = 22;
+const floating = 22.223;
+const anotherNumber = 10;
+let age = 23;
+let sum = myNumber + anotherNumber;
+// console.log(sum);
+console.log(typeof floating);
+
+// BOOLEANS
+const booleanOne = true;
+const booleanTwo = false;
+console.log(typeof booleanOne);
+
+let x;
+// x = 1;
+// console.log(x);
