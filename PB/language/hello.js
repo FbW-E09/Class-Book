90624b1a19083510d3916e6a8e9d740b219936fb
HelonDCI <william.roop@digitalcareerinstitute.org>
2022-01-11 06:31:53 -0300

    Added language files

---
+++ PB/language/hello.js
@@ -0,0 +1,5 @@
+// Logging text to the console
+// We need parentheses
+
+console.log('hello world'); // This is a JS statement, they should end in a semi colon
+console.log(1 + 2);
---
+++ PB/language/variables.js
@@ -0,0 +1,28 @@
+// JS Variables (how we can save/store data)
+// How to define a variable:
+// 1. const
+// 2. let
+// 3. var
+// Basically, never use var
+// Always use const
+
+const greeting = 'hello';
+const firstName = 'helon';
+const lastName = 'roop';
+// Standard is to use camelCase
+// camel case: studentName
+// snake case: student_name
+// console.log(greeting);
+// console.log(firstName);
+// console.log(lastName);
+
+// Declare a variable with let if you intend to change the value
+let randomPerson = 'Bert';
+console.log(randomPerson);
+randomPerson = 'Ernie';
+console.log(randomPerson);
+const test = 'Bert' + 1;
+console.log(test);
+// It works but don't use it
+// var movieStar = 'Nic Cage';
+// console.log(movieStar);
