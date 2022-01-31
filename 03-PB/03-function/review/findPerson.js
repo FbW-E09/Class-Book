5ba807c42509631cda42d155ff9da70d96b9b8f9
HelonDCI <william.roop@digitalcareerinstitute.org>
2022-01-31 08:06:16 -0300

    Added more review

--- 03-PB/03-function/review/findPerson.js
+++ 03-PB/03-function/review/findPerson.js
@@ -1,15 +1,38 @@
 // Given a name, return true if the array we have contains that name
 
-const people = ['Helon', 'Keanu', 'Nic'];
+const people = ['Keanu', 'Helon', 'Nic'];
+// 'hElOn' === 'Helon' --> true
+// 'helon' === 'HELON' --> true
+// 'helon' === 'helon' --> true
 
 function findPersonByName(name) {
   const lowerCasedName = name.toLowerCase();
   // loop over the array
   for (let i = 0; i < people.length; i++) {
     //   check if any of the names match name passed in
-    console.log(people[i]);
+    if (people[i].toLowerCase() === lowerCasedName) {
+      return true;
+    }
   }
+  return false;
   // return true if so, or false if not
 }
 
-console.log(findPersonByName('hElOn'));
+const findMe = (me, arr) =>
+  arr.join(' ').toLowerCase().includes(me.toLowerCase());
+
+console.log(findMe('hELon', people));
+
+const findPerson = (person) => {
+  const lowerCasedName = person.toLowerCase();
+  for (let i = 0; i < people.length; i++) {
+    //   check if any of the names match name passed in
+    const currPerson = people[i].toLowerCase();
+    if (currPerson === lowerCasedName) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// console.log(findPerson('hELOn'));
